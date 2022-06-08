ee123b4166fb02b7a6c483045cf52be16e3e83d3
Seunggyeom Kim <fxf24@naver.com>
2022-06-08 17:33:52 +0900

    Make Custom tree

--- Console2/Console2/Tree.cpp
+++ Console2/Console2/Tree.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
-
+#include <map>
+#include <set>
 using namespace std;
 
 // 배열, 연결리스트 선형구조
 
 // 트리 비선형구조
+
+class Coord
+{
+	int x;
+	int y;
+};
+
+struct TreeNode
+{
+	int		  value;
+	TreeNode* Lchild = nullptr;
+	TreeNode* Rchild = nullptr;
+	void Print()
+	{
+		cout << value << "->";
+		if (Lchild)
+			Lchild->Print();
+		if (Rchild)
+			Rchild->Print();
+	}
+};
+
+class Tree
+{
+private:
+	TreeNode* Root = nullptr;
+	
+public:
+	void PrintAll()
+	{
+		if (Root)
+			Root->Print();
+	}
+};
+
 int main()
 {
+	map<Coord, int> a;
+	set<Coord> b;
 
+	
 
 	return 0;
 }
