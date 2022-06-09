c97cc008d3e71e30a4925ebda3e241102a9e3249
Seunggyeom Kim <fxf24@naver.com>
2022-06-09 17:50:50 +0900

    Make Tree Push & Working on Pop

--- Console2/Console2/Tree.cpp
+++ Console2/Console2/Tree.cpp
@@ -18,13 +18,71 @@ struct TreeNode
 	int		  value;
 	TreeNode* Lchild = nullptr;
 	TreeNode* Rchild = nullptr;
-	void Print()
+	TreeNode* Parent = nullptr;
+
+	void Push(TreeNode* child)
+	{
+		if (child->value > value)
+		{
+			if (Rchild) Rchild->Push(child);
+			else
+			{
+				Rchild = child;
+				child->Parent = this;
+			}
+		}
+		else if (child->value < value)
+		{
+			if (Lchild) Lchild->Push(child);
+			else
+			{
+				Lchild = child;
+				child->Parent = this;
+			}
+		}
+	}
+
+	TreeNode* Find(int data)
+	{
+		if (data == value) return this;
+		else if (data > value)
+		{
+			if (!Rchild)
+				Rchild->Find(data);
+		}
+		else if (data < value)
+		{
+			if (!Lchild)
+				Lchild->Find(data);
+		}
+
+		return nullptr;
+	}
+
+	void PreOrder()
 	{
 		cout << value << "->";
 		if (Lchild)
-			Lchild->Print();
+			Lchild->PreOrder();
 		if (Rchild)
-			Rchild->Print();
+			Rchild->PreOrder();
+	}
+	void InOrder()
+	{
+		if (Lchild)
+			Lchild->InOrder();
+		cout << value << "->";
+		if (Rchild)
+			Rchild->InOrder();
+
+	}
+	void PostOrder()
+	{
+		if (Lchild)
+			Lchild->PostOrder();
+		if (Rchild)
+			Rchild->PostOrder();
+		cout << value << "->";
 	}
 };
 
@@ -33,20 +91,106 @@ class Tree
 private:
 	TreeNode* Root = nullptr;
 	
+public: 
+	void Push(int data)
+	{
+		TreeNode* temp = new TreeNode();
+		temp->value = data;
+		if (!Root)
+		{
+			Root = temp;
+		}
+		else
+		{
+			Root->Push(temp);
+		}
+	}
+
+	void Pop(int data)
+	{
+		TreeNode* iter = Root->Find(data);
+
+		if (iter->Rchild && iter->Lchild)
+		{
+			if (iter->Parent->value > iter->value)
+			{
+				iter->Parent->Lchild = iter->Lchild;
+				iter->Lchild->Parent = iter->Parent;
+				iter->Lchild->Rchild = iter->Rchild;
+				iter->Rchild->Parent = iter->Lchild;
+				delete iter;
+			}
+			else if (iter->Parent->value < iter->value)
+			{
+				iter->Parent->Rchild = iter->Rchild;
+				iter->Rchild->Parent = iter->Parent;
+				iter->Rchild->Lchild = iter->Lchild;
+				iter->Lchild->Parent = iter->Rchild;
+				delete iter;
+			}
+		}
+		else if (iter->Rchild)
+		{
+			if (iter->Parent->value > iter->value)
+			{
+				
+				delete iter;
+			}
+			else if (iter->Parent->value < iter->value)
+			{
+
+				delete iter;
+			}
+		}
+	}
+
 public:
-	void PrintAll()
+	void PrePrint()
 	{
 		if (Root)
-			Root->Print();
+			Root->PreOrder();
+	}
+	void InPrint()
+	{
+		if (Root)
+			Root->InOrder();
+	}
+	void PostPrint()
+	{
+		if (Root)
+			Root->PostOrder();
 	}
 };
 
 int main()
 {
-	map<Coord, int> a;
-	set<Coord> b;
+	Tree a;
 
-	
+	while (1)
+	{
+		cout << "전위:"; a.PrePrint(); cout << endl;
+		cout << "중위:"; a.InPrint(); cout << endl;
+		cout << "후위:"; a.PostPrint(); cout << endl;
+
+		int Select;
+		cout << "1. 삽입 2. 삭제" << endl;
+		cin >> Select;
+
+		if (Select == 1)
+		{
+			int input;
+			cout << "삽입할 데이터를 입력해주세요:";
+			cin >> input;
+			a.Push(input);
+		}
+		else if (Select == 2)
+		{
+			int input;
+			cout << "삭제할 데이터를 입력해주세요 : ";
+			cin >> input;
+			a.Pop(input);
+		}
+	}
 
 	return 0;
 }
