4acc415f768e87ea32c5c04d0f6c029d1bb8c1b0
Seunggyeom Kim <fxf24@naver.com>
2022-08-11 17:46:12 +0900

    Add algorithm snail

---
+++ Algorithm/Algorithm/Snail.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n, m;
+
+	cin >> n >> m;
+
+	vector<vector<int>> v(n, vector<int>(m, 0));
+
+	
+
+	return 0;
+}
--- Algorithm/Algorithm/Town.cpp
+++ Algorithm/Algorithm/Town.cpp
@@ -2,39 +2,42 @@
 #include <fstream>
 #include <vector>
 #include <queue>
+#include <chrono>
+#include <string>
 using namespace std;
 
 
 class Answer
 {
 public:
-	int answer(ifstream& fin)
+	int answer(string name)
 	{
+		ifstream fin;
+		fin.open(name);
+
 		int answer = 1;
 
 		vector<vector<int>> v(60001, vector<int>(0,0));
 		int visited[60001] = { 0, };
-		while (!fin.eof())
-		{
-			int num;
-			fin >> num;
 
-			for (int i = 0; i < num; i++)
+		int num;
+		fin >> num;
+		for (int i = 0; i < num; i++)
+		{
+			int n;
+			fin >> n;
+			int m;
+			fin >> m;
+			for (int j = 0; j < m; j++)
 			{
-				int n;
-				fin >> n;
-				int m;
-				fin >> m;
-				for (int j = 0; j < m; j++)
-				{
-					int k;
-					fin >> k;
-					v[n].push_back(k);
-					//cout << v[n][j] << " ";
-				}
-				//cout << endl;
+				int k;
+				fin >> k;
+				v[n].push_back(k);
+				//cout << v[n][j] << " ";
 			}
+			//cout << endl;
 		}
+
 		fin.close();
 
 		queue<pair<int, int>> q;
@@ -66,26 +69,15 @@ public:
 int main()
 {
 	Answer a;
-	ifstream fin1;
-	fin1.open("./마을1.txt");
 
-	if (fin1.fail())
-	{
-		cout << "error : fin1" << endl;
-	}
-
-	ifstream fin2;
-	fin2.open("./마을2.txt");
-	if (fin2.fail())
-	{
-		cout << "error : fin2" << endl;
-	}
-
-	int answer1 = a.answer(fin1);
-	int answer2 = a.answer(fin2);
+	int answer1 = a.answer("./마을1.txt");
+	chrono::system_clock::time_point start = std::chrono::system_clock::now();
+	int answer2 = a.answer("./마을2.txt");
 
 	cout << answer1 << endl;
 	cout << answer2 << endl;
+	chrono::duration<double>sec = std::chrono::system_clock::now() - start;
+	cout << sec.count() << "seconds"<< endl;
 
 	while (true)
 	{
